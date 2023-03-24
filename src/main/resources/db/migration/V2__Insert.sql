74383e3b155b4c4303980d6c4245898c8284fde1
Elvira Sumarokava <eltimka@gmail.com>
2023-03-24 17:01:01 -0400

    Initial commit

---
+++ src/main/resources/db/migration/V2__Insert.sql
@@ -0,0 +1,5 @@
+insert into person (id, name) values ('c2d29867-3d0b-d497-9191-18a9d8ee7830', 'one');
+insert into person (id, name) values ('c2d29867-3d0b-d497-9191-18a9d8ee7831', 'two');
+insert into person (id, name) values ('c2d29867-3d0b-d497-9191-18a9d8ee7832', 'three');
+insert into person (id, name) values ('c2d29867-3d0b-d497-9191-18a9d8ee7833', 'four');
+insert into person (id, name) values ('c2d29867-3d0b-d497-9191-18a9d8ee7834', 'five');
